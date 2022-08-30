bb820de12f5e2c8dc2a8305a220c98f1a5114e36
Tarei King <tarei@futurelab.co.nz>
2022-08-30 19:42:23 +1200

    Initial commit

---
+++ stories/site.stories.js
@@ -0,0 +1,7 @@
+export default {
+  title: 'Site',
+};
+
+export const Heading = () => require('../resources/html/site/header.html')
+
+export const Markdown = () => require('../resources/html/site/markdown.html')
